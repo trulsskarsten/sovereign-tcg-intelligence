12fa65f758cc92b1306f7445bd87fd19081c8c5c
Truls Skarsten <truls_skarsten@hotmail.com>
2026-04-11 01:26:03 +0200

    fix: add missing formatCurrency utility for build

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('nb-NO', {
+    style: 'currency',
+    currency: 'NOK',
+  }).format(amount);
+}
